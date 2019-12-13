8c002f0249ed917180032d3a45caee82f84b18a7
Raymond Mendelovits <dataaskdatatell@outlook.com>
2019-12-13 06:40:48 -0500

    Add strings to basic

--- autocoder-server/routes/basic.js
+++ autocoder-server/routes/basic.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const parseString = require('xml2js').parseString;
+const utils = require('./utils.js');
 
-/* GET home page. */
+/* GET basics page. */
 router.get('/', function(req, res, next) {
-  res.render('basic/basic', { title: 'Autocoder' });
+  fs.readFile(path.join(__dirname, '../views/basic/strings.xml'), 'utf-8', function (error, xml) {
+    utils.catchError(error, 'The dynamic strings failed to load', req, res);
+    parseString(xml, function (err, result) {
+      utils.catchError(err, 'Could not parse dynamic strings', req, res);
+      const resources = result['resources']['string'];
+      res.render('basic/basic', {
+        title: utils.getProperty('title', resources, req, res),
+        problemTitle: utils.getProperty('problem_title', resources, req, res),
+        problemDescription: utils.getProperty('problem_description', resources, req, res),
+        inputSpeciﬁcation: utils.getProperty('input_speciﬁcation', resources, req, res),
+        outputSpeciﬁcation: utils.getProperty('output_speciﬁcation', resources, req, res),
+        numberOfTests: utils.getProperty('number_of_tests', resources, req, res),
+        submitButtonLabel: utils.getProperty('submit_button_label', resources, req, res),
+      });
+    });
+  });
 });
 
 module.exports = router;
