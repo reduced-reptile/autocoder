ea6460e0d2469f999fc5a8bdd3080bc4ece77fe3
Raymond Mendelovits <dataaskdatatell@outlook.com>
2019-12-25 23:17:18 -0500

    replace sample problems with legitimate ones

--- autocoder-package/tests/index.test.js
+++ autocoder-package/tests/index.test.js
@@ -1,39 +1,11 @@
 const autocoder = require('../built');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
-const problems = [
-  {
-    step: 'code',
-    lang: 'en',
-    problemTitle: 'problem title 1',
-    problemDescription: 'problem description 1',
-    inputSpeciﬁcation: 'input speciﬁcation 1',
-    outputSpeciﬁcation: 'output speciﬁcation 1',
-    programmingLanguage: 'crystal',
-    codeOutput: 'puts "Hello world!"'
-  },
-  {
-    step: 'code',
-    lang: 'en',
-    problemTitle: 'problem title 2',
-    problemDescription: 'problem description 2',
-    inputSpeciﬁcation: 'input speciﬁcation 2',
-    outputSpeciﬁcation: 'output speciﬁcation 2',
-    programmingLanguage: 'crystal',
-    codeOutput: 'puts "Hello world!"'
-  },
-  {
-    step: 'code',
-    lang: 'en',
-    problemTitle: 'problem title 3',
-    problemDescription: 'problem description 3',
-    inputSpeciﬁcation: 'input speciﬁcation 3',
-    outputSpeciﬁcation: 'output speciﬁcation 3',
-    programmingLanguage: 'crystal',
-    codeOutput: 'puts "Hello world!"'
-  },
-];
-
-problems.forEach((problem) => {
+try {
+  const fileContents = fs.readFileSync('tests/problems.yaml', 'utf8');
+  const problems = yaml.safeLoadAll(fileContents);
+  problems.forEach((problem) => {
     test(`creates the correct code for ${problem.problemTitle}`, () => {
         expect(autocoder(problem.step, problem.lang, {
             problemTitle: problem.problemTitle,
@@ -46,4 +18,9 @@ problems.forEach((problem) => {
             code: problem.codeOutput
         });
     });
-});
+  });
+} catch (e) {
+  test('Fail if an exception occurs', () => {
+    expect(true).toBe(e);
+  });
+}
